fa91539e9e2a8e2ec0bd0047a0f7deee2e3c03e8
Shinie Kie Avila-Dangasi <shiniekie.dangasi@nmsc.edu.ph>
2026-03-07 21:27:38 +0800

    Add files via upload

--- jQuery.js
+++ jQuery.js
@@ -1,36 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $("#solveBtn").click(function(){
+    $("#solveBtn").click(function() {
 
         var num1 = parseFloat($("#num1").val());
         var num2 = parseFloat($("#num2").val());
         var operator = $("#operator").val();
-        var answer;
+        var result;
 
         if (isNaN(num1) || isNaN(num2)) {
-            $("#result").text("Please enter both numbers.");
+            alert("Please enter both numbers.");
             return;
         }
 
-        if (operator === "+") {
-            answer = num1 + num2;
-        } 
-        else if (operator === "-") {
-            answer = num1 - num2;
-        } 
-        else if (operator === "*") {
-            answer = num1 * num2;
-        } 
-        else if (operator === "/") {
-            if (num2 === 0) {
-                $("#result").text("Cannot divide by zero.");
-                return;
-            }
-            answer = num1 / num2;
+        switch(operator) {
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/":
+                result = (num2 === 0) ? "Cannot divide by zero" : num1 / num2;
+                break;
         }
 
-        $("#result").text("Result: " + answer);
-
+        // Show result with slight indentation
+        $("#result").text("Result: " + result);
     });
 
 });
